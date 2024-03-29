703a70ca6537f4627256f9f9348be64e08713d11
yxsi <60534039+syx0310@users.noreply.github.com>
2024-03-29 16:22:31 +0800

    加点注释

--- controller/instance.go
+++ controller/instance.go
@@ -8,27 +8,32 @@ import (
 )
 
 func StartInstanceByInstanceID(c *gin.Context) {
+	// 传入一个id，根据id启动一个实例
 
 }
 
 func StopInstanceByInstanceID(c *gin.Context) {
+	// 传入一个id，根据id停止一个实例
 
 }
 
 func RemoveInstancerByInstanceID(c *gin.Context) {
+	// 传入一个id，根据id删除一个实例
 
 }
 
 func CreateInstanceConfigAndStart(c *gin.Context) {
+	// 传入一个配置并启动
+	// 传入的配置是一个json，包含了实例的配置信息，格式未定，按照需要加键值对
 
 }
 
 func EditInstanceConfig(c *gin.Context) {
-
+	// 传入一个id和一个修改后的配置json，根据id修改配置
 }
 
 func ExportInstanceImage(c *gin.Context) {
-
+	// 获取container的id->利用其他机子上的程序调用nerdctl commit并且push到仓库
 }
 
 func GetAllAvailableInstanceConfig(c *gin.Context) {
