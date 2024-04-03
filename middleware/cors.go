070bac673f7efd04795144ca79b61cd8c8cdae0b
yxsi <60534039+syx0310@users.noreply.github.com>
2024-04-03 11:34:24 +0800

    cors setting

--- middleware/cors.go
+++ middleware/cors.go
@@ -7,6 +7,7 @@ import (
 
 func CORS() gin.HandlerFunc {
 	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"https://gin-template.vercel.app", "http://localhost:3000/"}
+	// config.AllowOrigins = []string{"https://gin-template.vercel.app", "http://localhost:3000/"}
+	config.AllowAllOrigins = true
 	return cors.New(config)
 }
