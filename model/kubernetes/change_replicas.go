104f1f536d878e5942399ffa0872844b33b9d7ef
mrxYan6 <1827732507@qq.com>
2024-04-06 14:36:49 +0800

    fix bug

--- model/kubernetes/change_replicas.go
+++ model/kubernetes/change_replicas.go
@@ -2,18 +2,30 @@ package kubernetes
 
 import (
 	"context"
+	"fmt"
 	"gin-template/common"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
-func ChangeReplicas(namespace string, name string, replicas int) error {
+func ChangeReplicas(name string, namespace string, replicas int) error {
 	config := GetConf()
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		common.SysLog(err.Error())
 		return err
+	}
+	common.SysLog(fmt.Sprintf("ns: %s, name: %s, replicas: %d", namespace, name, replicas))
+	// pirnt all statefulsets:
+	{
+		ss, _ := clientset.AppsV1().StatefulSets(namespace).List(context.TODO(), metav1.ListOptions{})
+		// if err != nil {
+		// 	common.SysLog(err.Error())
+		// 	return err
+		for _, s := range ss.Items {
+			common.SysLog(fmt.Sprintf("ns: %s, name: %s, replicas: %d", s.Namespace, s.Name, *s.Spec.Replicas))
+		}
 	}
 	ss, err := clientset.AppsV1().StatefulSets(namespace).Get(context.TODO(), name, metav1.GetOptions{})
 
